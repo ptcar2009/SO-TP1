dd629efff8a96604ef661363f7a5b9b7265931b6
Pedro <ptcar@inspr.com>
2021-01-10 11:03:02 -0300

    começando a implementaçao do sistema de dependencias

--- main.cpp
+++ main.cpp
@@ -1,68 +1,141 @@
 #include <iostream>
 #include <cstdlib>
 #include <pthread.h>
-#include <thread>         // std::this_thread::sleep_for
-#include <chrono>         // std::chrono::seconds
+#include <thread> // std::this_thread::sleep_for
+#include <chrono> // std::chrono::seconds
 
-#define NUM_THREADS 2
+#define NUM_PERSONAGENS 9
+#define lock(a) pthread_mutex_lock(a)
 
-// Global variables
-pthread_t threads[NUM_THREADS];
-pthread_mutex_t lock;
-int inUse = 0;
+typedef struct monitor
+{
+    monitor()
+    {
+        // Initialize mutex
+        if (pthread_mutex_init(&mutex, NULL) != 0)
+        {
+            std::cout << "Mutex init failed" << std::endl;
+            return;
+        }
 
+        for (int i = 0; i < NUM_PERSONAGENS; i++)
+        {
+            int threadError = pthread_create(&threads[i], NULL, ir_ao_trabalho, &i);
 
-struct thread_data{
-   int  thread_id;
-   int  accompanied;
-};
+            if (threadError)
+            {
+                std::cout << "Unable to create thread, " << threadError << std::endl;
+                exit(-1);
+            }
+        }
 
-void *useMicrowave(void *threadID) {
-    long tID = (long)threadID;
-    std::cout << "Going to heat food! Thread ID, " << tID << std::endl;
+        for (int i = 0; i < NUM_PERSONAGENS; i++)
+        {
+            pthread_join(threads[i], NULL);
+        }
 
-    pthread_mutex_lock(&lock);
-
-    if(inUse == 1) {
-        std::cout << "Waiting! Thread ID, " << tID << std::endl;
-    } else {
-        inUse = 1;
-        std::cout << "Heating up! Thread ID, " << tID << std::endl;
-        std::this_thread::sleep_for (std::chrono::seconds(5));
-        inUse = 0;
-        std::cout << "Done heating! Thread ID, " << tID << std::endl;
+        pthread_mutex_destroy(&mutex);
+        pthread_exit(NULL);
     }
-    
-    pthread_mutex_unlock(&lock);
-    pthread_exit(NULL);
+    static thread_data pers[9];
+    static pthread_t threads[9];
+    static pthread_mutex_t mutex;
+
+    static void useMicrowave(int);
+    static void releaseMicrowave(int);
+
+} monitor;
+
+typedef struct thread_data
+{
+    std::string name;
+    int thread_id;
+    bool acc;
+    int count_forno;
+    pthread_cond_t cond;
+    bool running, fila;
+
+} thread_data;
+
+void monitor::useMicrowave(int tID)
+{
+    std::cout << pers[tID].name << " quer usar o forno." << std::endl;
+    if (tID < 3 && pers[tID + 5].fila)
+        pers[tID + 5].acc = pers[tID].acc = true;
+
+    if (tID > 4 && tID < 8 && pers[tID - 5].fila)
+        pers[tID - 5].acc = pers[tID].acc = true;
+
+    pers[tID].fila = true;
+
+    if (tID < 3)
+    {
+        int bp = (tID + 1) % 3;
+        int sp = (tID - 1 >= 0 ? tID - 1 : 2);
+        if (pers[tID].acc)
+        {
+            if ((pers[bp].acc && pers[bp].fila) || pers[bp + 5].acc && pers[bp + 5].fila)
+                pthread_cond_wait(&pers[tID].cond, &mutex);
+        }
+        else
+        {
+            if (((pers[sp].acc && pers[sp].fila) || (pers[sp + 5].acc && pers[sp + 5].fila)) || (pers[bp].fila || pers[bp + 5].fila))
+                pthread_cond_wait(&pers[tID].cond, &mutex);
+        }
+    }
+    else if (tID == 3)
+    {
+        bool cond = false;
+        for (int i = 0; i < 3; cond |= pers[i++].fila)
+            ;
+        for (int i = 5; i < 8; cond |= pers[i++].fila)
+            ;
+        if (cond)
+            pthread_cond_wait(&pers[tID].cond, &mutex);
+    }
+    else if (tID == 4)
+    {
+        bool cond = false;
+        for (int i = 0; i < 4; cond |= pers[i++].fila)
+            ;
+        for (int i = 5; i < 8; cond |= pers[i++].fila)
+            ;
+        if (cond)
+            pthread_cond_wait(&pers[tID].cond, &mutex);
+    }
+    else if (tID < 8)
+    {
+        int bp = (tID - 4) % 3 + 5;
+        int sp = (tID - 6 >= 0 ? tID - 1 : 2) + 5;
+        if (pers[tID].acc)
+        {
+            if ((pers[bp].acc && pers[bp].fila) || pers[bp - 5].acc && pers[bp - 5].fila)
+                pthread_cond_wait(&pers[tID].cond, &mutex);
+        }
+        else
+        {
+            if (((pers[sp].acc && pers[sp].fila) || (pers[sp - 5].acc && pers[sp - 5].fila)) || (pers[bp].fila || pers[bp - 5].fila))
+                pthread_cond_wait(&pers[tID].cond, &mutex);
+        }
+    }
+    pthread_mutex_lock(&mutex);
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+}
+
+void monitor::releaseMicrowave(int tID)
+{
+}
+
+void *ir_ao_trabalho(void *tID)
+{
+    int ID = *((int *)tID);
+    int nIter = (&ID)[1];
+    monitor::useMicrowave(ID);
+    monitor::releaseMicrowave(ID);
 }
 
 // Main
-int main(int argc, char** argv) {
-    // Need to implement error treatment
-    int useTimes = std::stoi(argv[1]);
-
-    // Initialize mutex
-    if (pthread_mutex_init(&lock, NULL) != 0) {
-        std::cout << "Mutex init failed" << std::endl;
-        return 1;
-    }
-
-    for(int i=0; i < NUM_THREADS; i++) {
-        // std::cout << "Creating thread, " << i << std::endl;
-        int threadError = pthread_create(&threads[i], NULL, useMicrowave, (void*) i);
-      
-        if(threadError) {
-            std::cout << "Unable to create thread, " << threadError << std::endl;
-            exit(-1);
-        }
-    }
-
-    for(int i=0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i], NULL);
-    }
-    
-
-    pthread_mutex_destroy(&lock);
-    pthread_exit(NULL);
+int main(int argc, char **argv)
+{
 }
